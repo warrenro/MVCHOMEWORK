a383593a7bc1949bdcdc11076aed33bf0c12b12f
warrenro <warren.ro@gmail.com>
2017-06-16 23:46:23 +0800

    first commit

---
+++ WebApplication9/Controllers/DataController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication9.Models.ViewModel;
+
+namespace WebApplication9.Controllers
+{
+    public class DataController : Controller
+    {
+        // GET: Data
+        public ActionResult Input()
+        {
+            return View();
+
+        }
+
+
+        //public ActionResult Output()
+        //{
+        //    var grid = new List<DataViewModel>
+        //    {
+        //        new DataViewModel {ID=1,Category="out",InputDate="2017/06/09",Money=1000,Message="AAA" },
+        //        new DataViewModel {ID=1,Category="out",InputDate="2017/06/09",Money=1000,Message="AAA" },
+        //        new DataViewModel {ID=1,Category="out",InputDate="2017/06/09",Money=1000,Message="AAA" }
+        //    };
+
+        //    return View(grid);
+        //}
+    }
+}
---
+++ WebApplication9/Models/ViewModel/DataViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+
+namespace WebApplication9.Models.ViewModel
+{
+    public class DataViewModel
+    {
+
+        [DisplayName("#")]
+        public int ID { get; set; }
+        [DisplayName("類別")]
+        public string Category { get; set; }
+        [DisplayName("金額")]
+        public int Money { get; set; }
+        [DisplayName("日期")]
+        public string InputDate { get; set; }
+        [DisplayName("備註")]
+        public string Message { get; set; }
+
+    }
+}
